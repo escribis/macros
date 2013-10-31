e19f634f4238f001c89b550c1a69f4028c33241d
megaplanet <megaplanet.github@megaplanet.org>
2013-10-31 14:44:33 +0100

    First pubic release
    Compatible with Modelio 2.2 and 3.0

--- CoExplorer.py
+++ CoExplorer.py
@@ -7,9 +7,16 @@
 #
 # Compatibility: Modelio 2.x, Modelio 3.x
 #
-# Purpose:
+# Target audience:
+#   This script mainly dedicated to developers who need to understand modelio Metamodel,
+#   for instance to develop scripts or modules. It allows the co-exploration of a model
+#   and the corresponding metamodel.
+#
+# Description:
 #   This macro allows to explore the set of selected elements by navigating at the
-#   same time in the model and the metamodel.
+#   same time in the model and the metamodel. While Modelio' model explorer and property
+#   sheets presents a "user oriented" view of the model, the CoExplorer will present
+#   a view strictly in line with the actual metamodel. 
 #   The set of all (non empty) features associated with each element is displayed,
 #   allowing the navigation to continue. Note that the tree is virtually infinite and
 #   that there is currently no indication that an element has been already visited.
@@ -23,26 +30,36 @@
 #   are not modeled, but the exploration is made possible.
 #
 # Installation:
-#   This script should be installed as a workspace macro with the following options:
+#   This script should be installed as a workspace macro using standard modelio procedure
+#   to add macros. The options to use are the following options:
 #      - "Applicable on" : No Selection. The macro is application on any kind of element
 #      - "Show in contextual menu" : YES
 #      - "Show in toolbar" : YES
-#   This script is based on the content of the "lib" directory which should be copied by
-#   hand to the macros directory of the workspace. That is, in the directory where this file
-#   CoExplorer.py is installed.
-#   Ultimately we should have
-#      CoExplorer.py
-#      lib/
-#          introspection.py
-#          misc.py
-#          res/
-#               assoc-1.gif
-#               assoc-n.gif
-#               ... all other images ...
+#   This script is based on the content of the "lib" directory which must be copied manually
+#   in the same directory as this very file. That is, in the directory where this file
+#   CoExplorer.py will be installed by modelio through the standard macro installation procedure.
+#   Ultimately we should have the following structure 
+#          CoExplorer.py             <--- this very file
+#             lib/
+#                 introspection.py
+#                 misc.py
+#                 ...                <--- possibly other jython modules
+#                 res/
+#                     assoc-1.gif
+#                     assoc-n.gif
+#                     ...            <--- other resources.
+#
+# History
+#   Version 1.0 - October 31, 2013
+#      - first public realease
 
 DEVELOPEMENT_MODE = True
 
 #---- add the "lib" directory to the path
+# The code below compute this path and put it in the variable SCRIPT_LIBRARY_DIRECTORY. 
+# Feel free to change it if necessary. Currently the "lib" directory is search within
+# the directory of this very file, but if you may want to change its location you can
+# uncomment the line defining SCRIPT_LIBRARY_DIRECTORY with an absolute path
 try:
   from org.modelio.api.modelio import Modelio
   orgVersion = True
@@ -50,12 +67,18 @@ except:
   orgVersion = False
 import os
 import sys 
-WORKSPACE_PATH=Modelio.getInstance().getContext().getWorkspacePath()
+WORKSPACE_DIRECTORY=Modelio.getInstance().getContext().getWorkspacePath().toString()
 if orgVersion:
-  SCRIPT_LIBRARY_PATH=os.path.join(WORKSPACE_PATH.toString(),'macros','lib')
+  MACROS_DIRECTORY=os.path.join(WORKSPACE_DIRECTORY,'macros')
 else:
-  SCRIPT_LIBRARY_PATH=os.path.join(WORKSPACE_PATH.toString(),'.config','macros','lib')
-sys.path.append(SCRIPT_LIBRARY_PATH)
+  MACROS_DIRECTORY=os.path.join(WORKSPACE_DIRECTORY,'.config','macros')
+SCRIPT_LIBRARY_DIRECTORY=os.path.join(MACROS_DIRECTORY,'lib')
+# Uncomment this line and adjust it if you want to put the "lib" directory somewhere else
+# SCRIPT_LIBRARY_DIRECTORY = "C:\\MODELIO3-WORKSPACE\\macros\\lib"
+print SCRIPT_LIBRARY_DIRECTORY
+sys.path.append(SCRIPT_LIBRARY_DIRECTORY)
+print sys.path
+
 
 
 #----- imports (reload in development mode)
